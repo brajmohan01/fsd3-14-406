07c17dfd773fbc5b6b7d3f326bd5b0f831351b27
Braj Mohan Singh <shivasinghj8.2001@gmail.com>
2026-09-03 14:45:35 +0530

    Update package.json to use prg11.js and add server implementation in prg11.js with various HTTP methods; enhance README with status codes and server capabilities

---
+++ lab3/prg11.js
@@ -0,0 +1,44 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/' && req.method === 'GET')
+        res.end('Home Page');
+    else if (req.url === '/product' && req.method === 'GET') {
+        const products = [
+            { id: 1, name: 'mobile', price: 10 },
+            { id: 2, name: 'duster', price: 20 }
+        ];
+        res.end(JSON.stringify(products));
+    }
+    else if (req.url === '/product' && req.method === 'POST'){
+        // retrive data from request body
+        let body ;
+        req.on('data', (chunk) => {
+            body += chunk;
+        });
+        req.on('end', (chunk) => {
+            const product = JSON.parse(body);
+            res.end(`Product ${product.name} added successfully`);
+        });
+
+        // add data to database
+        res.writeHead(201,{
+            "content-type": "application/json"
+        })
+
+        // send back the status
+        res.end(JSON.stringify({message: 'Product added successfully', product: product, status: 201}));
+
+        res.end('add Page');
+    }
+    else if (req.url === '/product' && req.method === 'PUT')
+        res.end('update Page');
+    else if (req.url === '/product' && req.method === 'DELETE')
+        res.end('delete Page');
+    else {
+        res.statusCode = 404;
+        res.end('Page Not Found');
+    }
+});
+
+server.listen(3001, () => {console.log('prg11 is running on port 3001')});
--- lab3/readme.md
+++ lab3/readme.md
@@ -10,3 +10,42 @@ it tells the browser what type of data is being sent. It is used in the header o
 - text/css: for CSS stylesheets
 - application/json: for JSON data
 - text/plain: for plain text files
+
+
+# Status codes
+- 200 -> OK
+- 201 -> Created
+- 202 -> Accepted
+- 204 -> No Content
+- 400 -> Bad Request
+- 401 -> Unauthorized
+- 402 -> Payment Required
+- 403 -> Forbidden
+- 404 -> Not Found
+- 500 -> Internal Server Error
+- 503 -> Service Unavailable
+
+# Server can send data
+1. html content
+2. html files
+3. json data
+4. plain text
+5. css
+6. jss
+7. file
+
+## Server can set Header to send data
+1. res.writeHeader()
+2. res.setHeader()
+
+## Server can set Status code
+1. res.statusCode()
+2. res.writeHeader()
+
+## request methods
+1. GET
+2. POST
+3. PUT/PATCH
+4. DELETE
+
+## routes
