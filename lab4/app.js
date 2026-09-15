96b1629f02af3524a6bd44dc6fdb43cfa2538c79
Braj Mohan Singh <shivasinghj8.2001@gmail.com>
2026-09-15 10:03:18 +0530

    Add DELETE and PUT endpoints for team management; implement team deletion and update functionality

--- lab4/app.js
+++ lab4/app.js
@@ -1,5 +1,5 @@
 import http from "http";
-import { getAllTeams, addTeam, getTeamById } from "./teams.js";
+import { getAllTeams, addTeam, getTeamById, deleteTeam , updateTeamById} from "./teams.js";
 import { parse as parseUrl } from "url";
 
 const PORT = 5002;
@@ -34,14 +34,9 @@ const server = http.createServer(async (req, res) => {
   console.log("Method:", method);
 
   if (pathname === "/api/v1/teams" && method === "GET") {
-
     let teams = getAllTeams();
     return sendJson(res, 200, teams, "count", teams.length);
-
-  } 
-  
-  else if (pathname === "/api/v1/teams" && method == "POST") {
-
+  } else if (pathname === "/api/v1/teams" && method == "POST") {
     const { tname, tl, members } = await parseJSONBody(req);
     if (!tname || !tl || !members)
       return sendJson(res, 400, {
@@ -50,12 +45,7 @@ const server = http.createServer(async (req, res) => {
     const team = addTeam({ tname, tl, members });
 
     return sendJson(res, 201, team, "Message", "Team registered successfully");
-
-  } 
-  
-  
-  
-  else if (pathname.startsWith("/api/v1/teams/") && method === "GET") {
+  } else if (pathname.startsWith("/api/v1/teams/") && method === "GET") {
     const id = Number(pathname.split("/").pop());
     const team = getTeamById(id);
 
@@ -64,6 +54,31 @@ const server = http.createServer(async (req, res) => {
         error: `Team with id: ${id} not found`,
       });
     return sendJson(res, 200, team, "Message", "Team Found");
+  } else if (pathname.startsWith("/api/v1/teams/") && method === "DELETE") {
+    const id = Number(pathname.split("/").pop());
+    const team = getTeamById(id);
+
+    if (!team)
+      return sendJson(res, 400, {
+        error: `Team with id: ${id} not found`,
+      });
+      deleteTeam(id);
+    return sendJson(res, 200, team, "Message", "Team Deleted successfully");
+
+  } else if (pathname.startsWith("/api/v1/teams/") && method === "PUT") {
+    const id = Number(pathname.split("/").pop());
+    const oldTeam = getTeamById(id);
+    if (!oldTeam)
+        return sendJson(res, 400, { error: `Team with id: ${id} not found` });
+    
+    const { tname, tl, members } = await parseJSONBody(req);
+    if (!tname || !tl || !members)
+      return sendJson(res, 400, {
+        error: "Team Name, Team Leader, or Members not defined",
+      });
+    const team = updateTeamById(id, { tname, tl, members });
+    return sendJson(res, 200, team, "Message", "Team Updated successfully");
+
   } else {
     res.statusCode = 404;
     res.end("Not matching");
